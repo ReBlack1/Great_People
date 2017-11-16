3a0b317bd1b428426d06b15c89af29cd77f80510
Евгений <krisa12300@yandex.ru>
2017-11-16 17:42:29 +0300

    Тесты окончены, если нужен будет API Гос думы, можно юзать эту прогу

--- GOSDUM_API_TEST.py
+++ GOSDUM_API_TEST.py
@@ -5,8 +5,9 @@ from lxml import etree
 API_KEY = "c3fee5876f89bd7d9cf05d87a0b21f2de7fa42ea"
 API_KEY_APP = "appf038c62e1646f599a6700e2df004d7ae4edcb6dd"
 
-url = "http://api.duma.gov.ru/api/" + API_KEY + "/deputies.xml?begin=А&app_token=" + API_KEY_APP
+url = "http://api.duma.gov.ru/api/" + API_KEY + "/deputies.xml?begin=В&current=1" + "&app_token=" + API_KEY_APP
+url = "http://api.duma.gov.ru/api/" + API_KEY + "/deputy.xml?id=99112983" + "&app_token=" + API_KEY_APP
 req = requests.get(url)
 html = etree.HTML(req.text)
 
-print(req.text)
+print(url)
