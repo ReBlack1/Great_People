bd1a097ee4caee58435a9030167be7e51359d988
Евгений <krisa12300@yandex.ru>
2017-11-18 22:44:04 +0300

    Сохранил данные о деятелях гос думы в csv (На обновление требуется около 15 минут)

--- Sum_To_CSV.py
+++ Sum_To_CSV.py
@@ -7,6 +7,9 @@ import Parser_Module
 import requests
 from lxml import etree
 
+def _makeNewBD():
+    _csvWriter([["Structure", "Second_Name", "First_Name", "Third_Name", "Birthday", "Birthmonth", "Birthyear", "Photo", "ID", "KEY"]])
+
 #Закидываешь туда массив из массивов и он делает из этого файлик с БД
 def _csvWriter(data):
     with open("BD.csv", "w", newline='') as csv_file:
@@ -27,9 +30,21 @@ def _csvAppend(data):
 
 def _UpgradeGosDum():
     ALFABET = ["А","Б","В","Г","Д","Е","Ж","З","И","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ш","Щ","Э","Ю","Я"]
-    for i in ALFABET[:4]:
+    for i in ALFABET:
         LIST_DEPUTY = Gos_Dum._GosDumSearch(i)
+        LIST_KEY_DEPUTY = []
+        LIST_DEPUTY_AGE = []
+        LIST_DEPUTY_PHOTO = []
+        SUM_LIST = []
+        for j in LIST_DEPUTY[0]:
+            LIST_KEY_DEPUTY.append(Gos_Dum._GosDumPersonGetKey(j))
+        for k in LIST_DEPUTY[1]:
+            LIST_DEPUTY_AGE.append(Gos_Dum._GosDumPersonAge(k))
+        for q in LIST_DEPUTY[1]:
+            LIST_DEPUTY_PHOTO.append(Gos_Dum._GosDumPersonImg(q))
+        for z in range(len(LIST_DEPUTY[0])):
+            SUM_LIST.append(["GD", LIST_DEPUTY[0][z].split()[0], LIST_DEPUTY[0][z].split()[1], LIST_DEPUTY[0][z].split()[2], LIST_DEPUTY_AGE[z][0],LIST_DEPUTY_AGE[z][1],
+            LIST_DEPUTY_AGE[z][2], LIST_DEPUTY_PHOTO[z], LIST_DEPUTY[1][z], LIST_KEY_DEPUTY[z]])
+        _csvAppend(SUM_LIST)
 
 
-
-_UpgradeGosDum()
